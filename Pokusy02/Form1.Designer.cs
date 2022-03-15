6907cc02ee5766490914d3965f25c7bbfc2c15fe
p24spst <p24.spst@gmail.com>
2022-03-15 08:26:09 +0100

    pokusz 01 a 02

--- Pokusy02/Form1.Designer.cs
+++ Pokusy02/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Pokusy01
+﻿namespace Pokusy02
 {
     partial class Form1
     {
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(177, 111);
+            this.button1.Location = new System.Drawing.Point(320, 62);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
--- Pokusy02/Form1.cs
+++ Pokusy02/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Pokusy01
+namespace Pokusy02
 {
     public partial class Form1 : Form
     {
@@ -19,7 +19,7 @@ namespace Pokusy01
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("01");
+            MessageBox.Show("02");
         }
     }
 }
